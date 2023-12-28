00e55f689ed8078a270556741ab36aa64fe8f6ef
Peacecraft <42166625+I3ushido@users.noreply.github.com>
2023-12-29 06:38:57 +0700

    feat: connect to mongodb & feature level (#1)

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,3 +1,45 @@
+require("dotenv").config();
+const {
+  Client,
+  IntentsBitField,
+  EmbedBuilder,
+  ActivityType,
+} = require("discord.js");
+
 module.exports = (client) => {
-  console.log(`${client.user.tag} is online 🟢`);
+  const STATUS = [
+    {
+      name: "Fireflies",
+      type: ActivityType.Streaming,
+      url: "https://www.youtube.com/watch?v=psuRGfAaju4",
+    },
+    {
+      name: "Bad Moon",
+      type: ActivityType.Streaming,
+      url: "https://www.youtube.com/watch?v=sZSdRVkplWQ",
+    },
+    {
+      name: "You Are The Reason",
+      type: ActivityType.Streaming,
+      url: "https://www.youtube.com/watch?v=ShZ978fBl6Y",
+    },
+    {
+      name: "Umbrella",
+      type: ActivityType.Streaming,
+      url: "https://www.youtube.com/watch?v=jUhMKjfc5yc",
+    },
+    {
+      name: "We Are Young",
+      type: ActivityType.Streaming,
+      url: "https://www.youtube.com/watch?v=Sv6dMFF_yts",
+    },
+  ];
+
+  console.log(`🟢 ${client.user.tag} is online`);
+
+  setInterval(() => {
+    const randomStatus = STATUS[Math.floor(Math.random() * STATUS.length)];
+    client.user.setActivity(randomStatus);
+    console.log(`🎵 ${randomStatus.name}`);
+  }, 1_000 * 60);
 };
