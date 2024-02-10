ba470ae32019a2b4f2a0b2897188fb975cf16f58
Peacecraft <42166625+I3ushido@users.noreply.github.com>
2024-02-10 14:42:02 +0700

    feat: refacttoring (#9)

--- src/helper/constance.js
+++ src/helper/constance.js
@@ -6,61 +6,61 @@ const {
   ActivityType,
 } = require("discord.js");
 
+const steaming = ActivityType.Streaming;
+const ROLES = {
+  ADMIN: "405396099524984843",
+  ADMIN_DEV: "1189546356457611274",
+};
+
+const STATUS = [
+  createStreamingStatus(
+    "Fireflies",
+    "https://www.youtube.com/watch?v=psuRGfAaju4"
+  ),
+  createStreamingStatus(
+    "Bad Moon",
+    "https://www.youtube.com/watch?v=sZSdRVkplWQ"
+  ),
+  createStreamingStatus(
+    "You Are The Reason",
+    "https://www.youtube.com/watch?v=ShZ978fBl6Y"
+  ),
+  createStreamingStatus(
+    "Umbrella",
+    "https://www.youtube.com/watch?v=jUhMKjfc5yc"
+  ),
+  createStreamingStatus(
+    "We Are Young",
+    "https://www.youtube.com/watch?v=Sv6dMFF_yts"
+  ),
+  createStreamingStatus("Older", "https://www.youtube.com/watch?v=r1Fx0tqK5Z4"),
+  createStreamingStatus(
+    "ハレハレヤ",
+    "https://www.youtube.com/watch?v=eg65SbqmT0s"
+  ),
+  createStreamingStatus(
+    "Change My Clothes",
+    "https://www.youtube.com/watch?v=kxWUcCUfDuE"
+  ),
+  createStreamingStatus(
+    "double take",
+    "https://www.youtube.com/watch?v=uQiF1yOnzDg"
+  ),
+  createStreamingStatus(
+    "drunk text",
+    "https://www.youtube.com/watch?v=OqEc_169ywY"
+  ),
+];
+
+function createStreamingStatus(name, url) {
+  return {
+    name,
+    type: steaming,
+    url,
+  };
+}
+
 module.exports = {
-  ROLES: {
-    ADMIN: "405396099524984843",
-    ADMIN_DEV: "1189546356457611274",
-  },
-  STATUS: [
-    {
-      name: "Fireflies",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=psuRGfAaju4",
-    },
-    {
-      name: "Bad Moon",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=sZSdRVkplWQ",
-    },
-    {
-      name: "You Are The Reason",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=ShZ978fBl6Y",
-    },
-    {
-      name: "Umbrella",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=jUhMKjfc5yc",
-    },
-    {
-      name: "We Are Young",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=Sv6dMFF_yts",
-    },
-    {
-      name: "Older",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=r1Fx0tqK5Z4",
-    },
-    {
-      name: "ハレハレヤ",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=eg65SbqmT0s",
-    },
-    {
-      name: "Change My Clothes",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=kxWUcCUfDuE",
-    },
-    {
-      name: "double take",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=uQiF1yOnzDg",
-    },
-    {
-      name: "drunk text ",
-      type: ActivityType.Streaming,
-      url: "https://www.youtube.com/watch?v=OqEc_169ywY",
-    },
-  ],
+  ROLES,
+  STATUS,
 };
